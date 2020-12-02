954538da173fa164ba20e05c0c9a1cce74f0a9a4
William Ganrot <william.ganrot@youcal.com>
2020-12-02 15:02:44 +0100

    day 2 solution 2

--- 02/solution.py
+++ 02/solution.py
@@ -2,23 +2,32 @@ passwordWithPolicy = []
 with open('input') as f:
     passwordWithPolicy = f.read().splitlines() 
 
-passwordWithPolicy = [i.split(':') for i in passwordWithPolicy]
-
 
 result = []
 for pair in passwordWithPolicy:
-    policy = pair[0]
-    password = pair[1]
     
-    character = policy[len(policy) - 1]
+    policy, password = pair.strip().replace(" ", "").split(':')
+    targetCharacter = policy[len(policy) - 1]
+    
     minApperance = int(policy[0 : policy.find('-')])
     maxApperance = int(policy[policy.find('-')+1 : policy.find(' ')])
 
-    #print(policy + ' ' + str(minApperance) + ' ' + str(maxApperance))
-
-
-    apperances = password.count(character)
+    apperances = password.count(targetCharacter)
     if(apperances <= maxApperance and apperances >= minApperance):
         result.append(password)
 
-print(len(result))
+print("Part1: " + str(len(result)))
+
+result = []
+for pair in passwordWithPolicy:  
+    
+    policy, password = pair.strip().replace(" ", "").split(':')
+    targetCharacter = policy[len(policy) - 1]
+    
+    lowIndex = int(policy[0 : policy.find('-')]) - 1
+    highIndex = int(policy[policy.find('-')+1 : policy.find(' ')]) - 1
+
+    if((password[lowIndex] == targetCharacter or password[highIndex] == targetCharacter) and password[lowIndex] is not password[highIndex]):
+        result.append(password)
+
+print("Part2: " + str(len(result)))
