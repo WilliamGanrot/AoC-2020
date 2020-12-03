a3b4b6812dc01ebf0f74f51a5bbc2872914395fb
William Ganrot <william.ganrot@youcal.com>
2020-12-03 13:01:36 +0100

    day 3

--- 03/solution.py
+++ 03/solution.py
@@ -1,18 +1,53 @@
 rows = []
 with open('input') as f:
     rows = f.read().splitlines() 
+length = len(rows[0])
+    
+def getX(y,right):
+    '''
+    x = right*y
+    while x >= length:
+        x = x - length
+    '''
+    print(str(right) + ' ' + str(y) + ' ' + str(((right*y)%length)))
+    x=((right*y)%length)
+    return x
+        
+def part1():
+    right = 3
+    c = 0
 
+    #trees = [for y, row in enumerate(rows) if str(rows[y][getX(y)]) == '#']
+    for y, row in enumerate(rows):
+        print(((right*y)%3))
+        if(str(rows[y][((right*y)%length)]) == '#'):
+            c += 1
 
+    print(c)
 
-def getChatacterAtXY(x,y):
-    return(rows[y][x]) #change possition of x, y to flip map
+def part2():
+    paths = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+    s = 1
+    for path in paths:
+        right = path[0]
+        height = path[1]
+        ctr = 0
+        y=0
+        x=0
+        while y < len(rows):
+            
+            if rows[y][x] == '#':
+                ctr += 1
+            x = (x + right)% length
+            y += height
 
-def calcRest():
+        s *= ctr
+
+    print(str(s))
+
     
 
-for y, row in enumerate(rows):
-    r = []
-    for x, c in enumerate(row):
-        r.append(getChatacterAtXY(x,y))
+  
 
-    print(r)
+#part1()
+part2()
