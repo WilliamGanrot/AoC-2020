93c247b97b41d44ecd1be986face9244042c4664
William Ganrot <william.ganrot@youcal.com>
2020-12-03 13:43:06 +0100

    pretty

--- 03/solution.py
+++ 03/solution.py
@@ -2,31 +2,9 @@ rows = []
 with open('input') as f:
     rows = f.read().splitlines() 
 length = len(rows[0])
-    
-def getX(y,right):
-    '''
-    x = right*y
-    while x >= length:
-        x = x - length
-    '''
-    print(str(right) + ' ' + str(y) + ' ' + str(((right*y)%length)))
-    x=((right*y)%length)
-    return x
-        
-def part1():
-    right = 3
-    c = 0
 
-    #trees = [for y, row in enumerate(rows) if str(rows[y][getX(y)]) == '#']
-    for y, row in enumerate(rows):
-        print(((right*y)%3))
-        if(str(rows[y][((right*y)%length)]) == '#'):
-            c += 1
 
-    print(c)
-
-def part2():
-    paths = [[1,1], [3,1], [5,1], [7,1], [1,2]]
+def slope(paths):
     s = 1
     for path in paths:
         right = path[0]
@@ -42,12 +20,12 @@ def part2():
             y += height
 
         s *= ctr
-
     print(str(s))
 
-    
+
 
   
+slope([[3,1]])
+slope([[1,1], [3,1], [5,1], [7,1], [1,2]])
 
-#part1()
-part2()
+ 
