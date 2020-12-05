9a92a853501db32ab0004655f6078d5c4d15af27
williamg <williamganrot@hotmail.com>
2020-12-05 11:36:25 +0100

    list stuff

--- 05/solution.py
+++ 05/solution.py
@@ -15,10 +15,8 @@ def part2(tickets):
     allSeats = list(range(0, 128*8))
     missingSeats = list(set(allSeats) - set(tickets))
 
-
-    for seat in missingSeats:
-        if seat-1 not in missingSeats and seat+1 not in missingSeats:
-            return seat
+    mySeat = [seat for seat in missingSeats if seat-1 not in missingSeats and seat+1 not in missingSeats]
+    return mySeat[0]
 
 print("part1: " + str(max([getTicketId(ticket) for ticket in tickets])))
 print("part2: " + str(part2([getTicketId(ticket) for ticket in tickets])))
