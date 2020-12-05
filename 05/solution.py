fd2ab3feb56c968717c4c0adfa7e9ec0f2ae699d
williamg <williamganrot@hotmail.com>
2020-12-05 11:31:53 +0100

    day 5

---
+++ 05/solution.py
@@ -0,0 +1,24 @@
+tickets = []
+with open('input') as f:
+    tickets = f.read().splitlines() 
+
+def getTicketId(ticket):
+    binaryRow = ticket[0:7].replace('F','0'). replace('B','1')
+    binaryCol = ticket[7:10].replace('R','1'). replace('L','0')
+
+    row = int(binaryRow, 2)
+    col = int(binaryCol, 2) 
+
+    return (row*8)+col
+
+def part2(tickets):
+    allSeats = list(range(0, 128*8))
+    missingSeats = list(set(allSeats) - set(tickets))
+
+
+    for seat in missingSeats:
+        if seat-1 not in missingSeats and seat+1 not in missingSeats:
+            return seat
+
+print("part1: " + str(max([getTicketId(ticket) for ticket in tickets])))
+print("part2: " + str(part2([getTicketId(ticket) for ticket in tickets])))
