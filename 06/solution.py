2d48853dbd5a773176a27d3b1a81d60608a13559
williamg <williamganrot@hotmail.com>
2020-12-06 09:05:07 +0100

    day 6

--- 06/solution.py
+++ 06/solution.py
@@ -1,10 +1,3 @@
-import string
-
-groups = []
-with open('input') as f:
-    groups = f.read().split('\n\n')
-    groups = [group.split('\n') for group in groups]
-
 def part1(group):
     yesAnswers = [answer for person in group for answer in person]
     distinctYesAnswers = list(''.join(set(yesAnswers)))
@@ -15,5 +8,9 @@ def part2(group):
     commonYesAnswers = list(set(membersYesAnswers[0]).intersection(*membersYesAnswers))
     return commonYesAnswers
 
-print("part1: " + str(sum([len(part1(group)) for group in groups])))
-print("part2: " + str(sum([len(part2(group)) for group in groups])))
+with open('input') as f:
+    groups = f.read().split('\n\n')
+    groups = [group.split('\n') for group in groups]
+    
+    print("part1: " + str(sum([len(part1(group)) for group in groups])))
+    print("part2: " + str(sum([len(part2(group)) for group in groups])))
